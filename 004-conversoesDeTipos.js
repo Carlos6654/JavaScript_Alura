6f584c37260e030656c9f5c06394736b806cc22b
Carlos Acácio de Campos Júnior <51809392+Carlos6654@users.noreply.github.com>
2022-02-14 15:14:11 -0300

    Add files via upload

---
+++ 004-conversoesDeTipos.js
@@ -0,0 +1,11 @@
+console.log("Conversão de Tipos");
+
+console.log("ano" + 2022);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+
+//conversão implícita com /,- e *
+console.log("10" / "2");
+console.log("Carlos" / "2"); //NaN Not a Number
+console.log("2" - "5");
+console.log("10" * "3");
